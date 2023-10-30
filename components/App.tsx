e4b4ec56eafa158f3fb4529a28165fad850071f0
Neel Raval <97961627+NeelRavalt@users.noreply.github.com>
2023-10-30 14:11:10 +0530

    Add files via upload

---
+++ components/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Function from './components/Function';
+import Class from './components/Class';
+function App() {
+  return (
+    <div className="App">
+  <Function name= 'neel'/>
+  <Class names= 'nilay'/>
+    </div>
+  );
+}
+
+export default App;
